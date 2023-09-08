46468f0fbda0553a7fe983fb0128a80de512a519
masond552 <mdivers67@charlottesvilleschools.org>
2023-09-08 14:07:44 -0400

    LAunchpadpush to github

--- raspberry-pi/LaunchPadPt1_Countdown.py
+++ raspberry-pi/LaunchPadPt1_Countdown.py
@@ -2,10 +2,13 @@
 import board
 import time
 
-print("Countdown")
-for x in range (10, 0, -1)
-    time.sleep(0.5)
-    time.sleep(1)
+print("Countdown")  # Print "Countdown" 
+time.sleep(1) # Pause for 1 second.
+
+for x in range(10, 0, -1): # Start a for loop with 'x' ranging from 10 down to 1 (inclusive), decreasing by 1 in each iteration.
+    time.sleep(1) # Pause for 1 second.
     print(x)
-if x == 1:
-    print("Liftoff")
+    
+    if x == 1:
+        time.sleep(1)
+        print("Liftoff")# Print "Liftoff" to the console when 'x' is equal to 1 
