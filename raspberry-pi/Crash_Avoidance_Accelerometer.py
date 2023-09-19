3709f19c3c043ef0c76ca98abeb24fea08ef9c9e
Mason Divers <91158978+MasonD552@users.noreply.github.com>
2023-09-19 10:13:54 -0400

    Create Crash_Avoidance_Accelerometer.py

---
+++ raspberry-pi/Crash_Avoidance_Accelerometer.py
@@ -0,0 +1,19 @@
+import board
+import busio
+import adafruit_mpu6050
+
+# Initialize I2C communication
+sda_pin = board.GP14  # Replace with your SDA pin
+scl_pin = board.GP15  # Replace with your SCL pin
+i2c = busio.I2C(scl_pin, sda_pin)
+
+# Initialize the MPU6050 sensor
+mpu = adafruit_mpu6050.MPU6050(i2c)
+
+while True:
+    # Read acceleration values
+    acceleration = mpu.acceleration
+    x_acceleration, y_acceleration, z_acceleration = acceleration
+    
+    # Print acceleration values to the serial monitor
+    print(f"Acceleration (m/s²) - X: {x_acceleration}, Y: {y_acceleration}, Z: {z_acceleration}")
